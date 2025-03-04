842ab6e11efe488b05584daa332dcad7951f9708
Antoine Le Calloch <71392407+antoine-le-calloch@users.noreply.github.com>
2025-03-04 16:26:36 -0600

    Improve the use of default scanning start date (#80)
    * Remove development mode date override in getStartDate function, because this function is only call when using a scanningProfile.
    
    * Refactor startDate initialization to use environment variable to set a default scanning start date.
    
    * Refactor config.js to only set config variable when development mode.
    
    * Refactor getDefaultValues to use config for startDate initialization.
    
    * Add VITE_SCANNING_START_DATE to README for pre-filled scanning form start date.

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 const mode = import.meta.env.MODE;
 
-export default {
-  SKIP_ONBOARDING:
-    mode === "development" && import.meta.env.VITE_SKIP_ONBOARDING === "true",
+const config = mode === "development" ? {
+  SKIP_ONBOARDING: import.meta.env.VITE_SKIP_ONBOARDING === "true",
   TOKEN: import.meta.env.VITE_SKYPORTAL_TOKEN,
   INSTANCE_URL: import.meta.env.VITE_INSTANCE_URL,
   INSTANCE_NAME: import.meta.env.VITE_INSTANCE_NAME,
   CLEAR_AUTH: import.meta.env.VITE_CLEAR_AUTH === "true",
-};
+  SCANNING_START_DATE: import.meta.env.VITE_SCANNING_START_DATE,
+} : {};
+
+export default config;
--- src/scanning/scanningLib.js
+++ src/scanning/scanningLib.js
@@ -95,6 +95,7 @@ import { useIonToast } from "@ionic/react";
 import { useCallback } from "react";
 import moment from "moment-timezone";
 import { SAVED_STATUS } from "../common/constants.js";
+import config from "../config.js";
 
 /**
  * @type {Object<ThumbnailType, ThumbnailType>}
@@ -456,10 +457,6 @@ export const parseIntList = (intListString) => {
  * @returns {string}
  */
 export const getStartDate = (scanningProfile) => {
-  if (import.meta.env.MODE === "development") {
-    return moment("2022-07-26T16:43:00-07:00").format();
-  }
-
   return moment()
     .subtract(parseInt(scanningProfile.timeRange), "hours")
     .format();
@@ -538,10 +535,7 @@ export const computeSavedStatus = ({
 };
 
 export const getDefaultValues = () => ({
-  startDate:
-    import.meta.env.MODE === "development"
-      ? moment("2022-07-26T16:43:00-07:00").format()
-      : moment().format(),
+  startDate: config.SCANNING_START_DATE || moment().format(),
   endDate: moment().format(),
   filterCandidates: false,
   filteringType: "include",
