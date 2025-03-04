a2bf082d9e0aa595dbd547fd56d08c4228b327f3
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-03 20:43:19 -0600

    fix bad retrieval name of env variable.

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export default {
   SKIP_ONBOARDING:
     mode === "development" && import.meta.env.VITE_SKIP_ONBOARDING === "true",
   TOKEN: import.meta.env.VITE_SKYPORTAL_TOKEN,
-  INSTANCE_URL: import.meta.env.VITE_INSTANCE_URL,
-  INSTANCE_NAME: import.meta.env.VITE_INSTANCE_NAME,
+  INSTANCE_URL: import.meta.env.VITE_SKYPORTAL_INSTANCE_URL,
+  INSTANCE_NAME: import.meta.env.VITE_SKYPORTAL_INSTANCE_NAME,
   CLEAR_AUTH: import.meta.env.VITE_CLEAR_AUTH === "true",
 };
